f634cd726cf543911438fdccf7f646592f658927
Link <50463727+link-discord@users.noreply.github.com>
2023-07-03 22:24:15 +0200

    Made pathfinder stop first

--- src/utils/stop.ts
+++ src/utils/stop.ts
@@ -2,8 +2,8 @@ import { CrystalBot } from '../types/CrystalBot.js'
 
 // Stops everything the bot is currently doing
 export function stop(bot: CrystalBot) {
+    bot.pathfinder.stop()
     bot.clearControlStates()
     bot.stopDigging()
     bot.deactivateItem()
-    bot.pathfinder.stop()
 }
